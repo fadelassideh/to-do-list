c6c4c3323c95fb9f5383fca8e9fcdf146aa5b402
Fadel Assideh <fadel_assideh@hotmail.com>
2021-11-03 01:57:42 +0300

    Databse configration / adding .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
+import {ConfigModule} from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController, TasksController],
   providers: [AppService],
 })
